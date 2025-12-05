20b8a1f370b10506f6e8c6bec4e2e55f8521f45a
JASA Essential <jasaessential3@gmail.com>
2025-12-05 22:04:54 +0000

    reduce the gap available on both top and down of the search bar in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -319,7 +319,7 @@ export default function Home() {
         )}
       </div>
 
-       <div className="container mx-auto px-4 py-4 mt-4">
+       <div className="container mx-auto px-4 py-2">
          <Popover open={isSearchOpen} onOpenChange={setIsSearchOpen}>
            <PopoverAnchor asChild>
             <div className="relative w-full">
