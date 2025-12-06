5bc5a5880d113b01fdaf073665cac2ba64a57243
JASA Essential <jasaessential3@gmail.com>
2025-12-06 04:34:01 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  devIndicators: {
+    allowedDevOrigins: [
+      "https://*.cloudworkstations.dev",
+    ],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
